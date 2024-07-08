55f07d9acb81524b8d190e832cb8cd4778c99ac7
Cosine-A <100404990+Cosine-A@users.noreply.github.com>
2024-07-08 20:20:41 +0900

    test: 가중치 랜덤 테스트

--- src/test/kotlin/kr/cosine/mythicmobsdrop/Test.kt
+++ src/test/kotlin/kr/cosine/mythicmobsdrop/Test.kt
@@ -1,6 +1,9 @@
 package kr.cosine.mythicmobsdrop
 
 import org.junit.jupiter.api.Test
+import java.lang.IllegalArgumentException
+import kotlin.math.ln
+import kotlin.random.Random
 import kotlin.reflect.full.isSubtypeOf
 import kotlin.reflect.full.starProjectedType
 import kotlin.reflect.jvm.jvmErasure
@@ -19,5 +22,28 @@ class Test {
             it.isSubtypeOf(Water::class.starProjectedType)
         }.arguments.firstOrNull()?.type?.jvmErasure
         println("clazz: $clazz")
+    }
+
+    @Test
+    fun chance_test() {
+        val map = mutableMapOf<String?, Double>(
+            "사과" to 2.0,
+            "바나나" to 5.0
+        )
+        val sum = 100 - map.values.sum()
+        map[null] = sum
+        val resultMap = mutableMapOf<String?, Int>()
+        repeat(100) {
+            val key = map.random()
+            val value = resultMap.getOrDefault(key, 0) + 1
+            resultMap[key] = value
+        }
+        println(resultMap)
+    }
+
+    private fun <T : Any?> Map<T, Double>.random(): T? {
+        return this.entries.minByOrNull {
+            -ln(Random.nextDouble()) / it.value
+        }?.key
     }
 }
