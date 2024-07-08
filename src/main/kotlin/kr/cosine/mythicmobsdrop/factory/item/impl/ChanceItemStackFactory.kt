381183a2c5d382b6da1dc9df6f45cc6832f5b161
Cosine-A <100404990+Cosine-A@users.noreply.github.com>
2024-07-08 20:22:32 +0900

    feat: 가중치 계산 시 임의 확률 추가

--- src/main/kotlin/kr/cosine/mythicmobsdrop/factory/item/impl/ChanceItemStackFactory.kt
+++ src/main/kotlin/kr/cosine/mythicmobsdrop/factory/item/impl/ChanceItemStackFactory.kt
@@ -1,8 +1,10 @@
 package kr.cosine.mythicmobsdrop.factory.item.impl
 
 import kr.cosine.mythicmobsdrop.data.drop.impl.ChanceDrop
+import kr.cosine.mythicmobsdrop.data.item.ChanceItemStack
 import kr.cosine.mythicmobsdrop.factory.item.BaseItemStackFactory
 import kr.hqservice.framework.global.core.component.Component
+import org.bukkit.Material
 import org.bukkit.inventory.ItemStack
 import kotlin.math.ln
 import kotlin.random.Random
@@ -13,9 +15,13 @@ class ChanceItemStackFactory : BaseItemStackFactory<ChanceDrop> {
     private val random: Random = Random
 
     override fun getItemStacks(drop: ChanceDrop): List<ItemStack> {
-        val itemStack = drop.baseItemStacks.minByOrNull {
+        val baseItemStacks = drop.baseItemStacks
+        val emptyBaseItemStackChance = 100 - baseItemStacks.sumOf { it.getChance() }
+        val emptyBaseItemStack = ChanceItemStack.of(ItemStack(Material.AIR), emptyBaseItemStackChance)
+        val itemStack = (baseItemStacks + emptyBaseItemStack).minByOrNull {
             -ln(random.nextDouble()) / it.getChance()
         }?.toOriginalItemStack() ?: return emptyList()
+        if (itemStack.type.isAir) return emptyList()
         return listOf(itemStack)
     }
 }
